a38d33fe6fc3a017923ca4f2679f54d70ef00681
CrispQATest <felix.diaz@crispmedia.com>
2015-05-15 00:51:14 -0400

    testing module integration

---
+++ src/Appium.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Codeception\Module;
+
+/**
+ * Description of Appium
+ *
+ * @author Felix Diaz
+ */
+use Codeception\Module;
+
+class Appium extends Module{
+    //put your code here
+public function testMessage(){
+    echo("TESTING");
+}
+    
+}
